d97422932e7fcd4ee66999377014a6b3432b6bf1
Tim Underwood <timunderwood@gmail.com>
2014-05-01 15:28:12 -0700

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,56 @@
+name := "fm-sbt-common"
+
+organization := "com.frugalmechanic"
+
+version := "0.1.0"
+
+description := "SBT Plugin with common settings used by Frugal Mechanic for both public and private projects"
+
+licenses := Seq("Apache License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt"))
+
+homepage := Some(url("https://github.com/frugalmechanic/fm-sbt-common"))
+
+sbtPlugin := true
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-proguard" % "0.2.2")
+
+resolvers += "Era7 maven releases" at "http://releases.era7.com.s3.amazonaws.com"
+
+// This currently isn't on maven central and needs the above resolver.
+// TODO: get this onto maven central so we aren't referencing some random maven repo!
+addSbtPlugin("ohnosequences" % "sbt-s3-resolver" % "0.10.1")
+
+publishMavenStyle := true
+
+publishTo <<= version { (v: String) =>
+  val nexus = "https://oss.sonatype.org/"
+  if (v.trim.endsWith("SNAPSHOT")) 
+    Some("snapshots" at nexus + "content/repositories/snapshots") 
+  else
+    Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+}
+
+publishArtifact in Test := false
+
+pomIncludeRepository := { _ => false }
+
+pomExtra := (
+  <developers>
+    <developer>
+      <id>tim</id>
+      <name>Tim Underwood</name>
+      <email>tim@frugalmechanic.com</email>
+      <organization>Frugal Mechanic</organization>
+      <organizationUrl>http://frugalmechanic.com</organizationUrl>
+    </developer>
+  </developers>
+  <scm>
+      <connection>scm:git:git@github.com:frugalmechanic/fm-lazyseq.git</connection>
+      <developerConnection>scm:git:git@github.com:frugalmechanic/fm-lazyseq.git</developerConnection>
+      <url>git@github.com:frugalmechanic/fm-lazyseq.git</url>
+  </scm>)
+
---
+++ src/main/scala/fm/sbt/FMCommon.scala
@@ -0,0 +1,158 @@
+package fm.sbt
+
+import sbt._
+import Keys._
+import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
+
+object FMCommon extends Plugin {
+  private val ProguardVersion: String = "4.11"
+  
+  private lazy val sharedSettings = Seq[Setting[_]](  
+    //
+    // Basic Project Settings
+    //
+    organization := "com.frugalmechanic",
+    
+    //
+    // Eclipse Plugin Settings
+    //
+    EclipseKeys.withSource := true,
+    
+    //
+    // Enable Sonatype repositories for SNAPSHOT versions only
+    //
+    resolvers <++= version { v: String =>
+      if (v.trim.endsWith("SNAPSHOT")) Seq(
+        "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+        "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases/"
+      ) else Nil
+    },
+    
+    //
+    // Publish Settings
+    //
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false }
+  )
+
+  // Reference this for Public projects
+  lazy val FMPublic = sharedSettings ++ Seq[Setting[_]](
+    //
+    // Basic Project Settings
+    //
+    homepage <<= (name){ projectName: String => Some(url(s"https://github.com/frugalmechanic/${projectName}")) },
+    licenses := Seq("Apache License, Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt")),
+    
+    //
+    // Publish Settings
+    //
+    publishTo <<= version { v: String =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots") 
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomExtra <<= (name){ projectName: String =>
+      <developers>
+        <developer>
+          <id>tim</id>
+          <name>Tim Underwood</name>
+          <email>tim@frugalmechanic.com</email>
+          <organization>Frugal Mechanic</organization>
+          <organizationUrl>http://frugalmechanic.com</organizationUrl>
+        </developer>
+      </developers>
+      <scm>
+          <connection>scm:git:git@github.com:frugalmechanic/{projectName}.git</connection>
+          <developerConnection>scm:git:git@github.com:frugalmechanic/{projectName}.git</developerConnection>
+          <url>git@github.com:frugalmechanic/{projectName}.git</url>
+      </scm>
+    }
+  )
+  
+  import ohnosequences.sbt.SbtS3Resolver._
+  
+  // This can be referenced by itself to enable the S3 resolver
+  lazy val FMS3Resolvers = S3Resolver.defaults ++ Seq[Setting[_]](
+    //
+    // S3 Setup
+    //
+    s3region := com.amazonaws.services.s3.model.Region.US_West_2,
+    s3credentials := {
+      file(System.getProperty("user.home")) / ".sbt" / ".frugalmechanic_maven_s3credentials" |
+      new com.amazonaws.auth.EnvironmentVariableCredentialsProvider() |
+      new com.amazonaws.auth.SystemPropertiesCredentialsProvider()
+    },
+    //
+    // Enable S3 repositories
+    //
+    resolvers <+= s3resolver { _("FrugalMechanic releases", s3("releases.maven.frugalmechanic.com")): Resolver },
+    resolvers <+= s3resolver { _("FrugalMechanic snapshots", s3("snapshots.maven.frugalmechanic.com")): Resolver }
+  
+    //resolvers <++= (version, s3resolver) { (v, resolver) =>
+    //  if (v.trim.endsWith("SNAPSHOT")) Seq[Resolver](resolver("FrugalMechanic snapshots", s3("snapshots.maven.frugalmechanic.com"))) else Nil
+    //}
+  )
+  
+  // Reference this for private projects
+  lazy val FMPrivate = sharedSettings ++ FMS3Resolvers ++ Seq[Setting[_]](    
+    //
+    // Publish to S3
+    //
+    publishTo <<= (version, s3resolver) { (v, resolver) =>
+      val prefix: String = if (v.trim.endsWith("SNAPSHOT")) "snapshots" else "releases"
+      Some(resolver("FrugalMechanic "+name, s3(prefix+".maven.frugalmechanic.com")))
+    }
+  )
+  
+  //
+  // Proguard Settings
+  //
+  // Modeled after https://github.com/rtimush/sbt-updates/blob/master/proguard.sbt
+  //
+  import com.typesafe.sbt.SbtProguard.{Proguard, ProguardKeys, proguardSettings}
+  
+  val publishMinJar = TaskKey[File]("publish-min-jar")
+  
+  // Dependencies marked as "embedded" will be used as program inputs to Proguard
+  val Embedded = config("embedded").hide
+  
+  lazy val `sbt-updates` = project in file(".") settings(
+    Project.defaultSettings ++
+    inConfig(Embedded)(Defaults.configSettings): _*
+  ) configs Embedded
+  
+  // This enables the default proguard settings plus adds our own
+  lazy val FMProguardSettings = proguardSettings ++ inConfig(Embedded)(Defaults.configSettings) ++ Seq[Setting[_]](
+    ivyConfigurations += Embedded,
+    
+    // Give the proguard process more memory
+    javaOptions in (Proguard, ProguardKeys.proguard) := Seq("-Xmx1024M", "-Dfile.encoding=UTF8"),
+    
+    // Use the Proguard Version specified at the top of this file
+    ProguardKeys.proguardVersion in Proguard := ProguardVersion,
+    
+    // Program inputs to Proguard are any dependencies in the "embedded" scope as well as our packaged Jar.
+    // All other dependencies will be used as library inputs.
+    ProguardKeys.inputs in Proguard <<= (dependencyClasspath in Embedded, packageBin in Runtime) map { 
+      (dcp, pb) => Seq(pb) ++ dcp.files
+    },
+    
+    publishMinJar <<= (ProguardKeys.proguard in Proguard) map { _.head }, 
+       
+    packagedArtifact in (Compile, packageBin) <<= (packagedArtifact in (Compile, packageBin), publishMinJar) map {
+      case ((art, _), jar) => (art, jar)
+    },
+    
+    // Add the dependencies marked "embedded" to the Compile and Test scopes
+    dependencyClasspath in Compile <++= dependencyClasspath in Embedded,
+    dependencyClasspath in Test <++= dependencyClasspath in Embedded,
+    
+    // We need the generated eclipse files to also include the "embedded" dependency libraries.
+    // Set(Compile, Test) is the default
+    EclipseKeys.configurations := Set(Compile, Test, Embedded)
+  )
+  
+}
